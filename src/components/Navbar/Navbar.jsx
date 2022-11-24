687bfb6c79ac92ddd70e7ca2c01abd0b3fc4b4f1
Desai Hardik <87645745+imhardikdesai@users.noreply.github.com>
2022-11-24 16:02:32 +0530

    created Navbar with logo in Chakra UI

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,63 @@
+import { ReactNode } from 'react';
+import { Box, Flex, HStack, Link, IconButton, useDisclosure, useColorModeValue, Stack, } from '@chakra-ui/react';
+
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+
+const Links = ['Home', 'About', 'Contact'];
+
+const NavLink = ({ children } = { children: ReactNode }) => (
+    <Link
+        px={2}
+        py={1}
+        rounded={'md'}
+        _hover={{
+            textDecoration: 'none',
+            bg: useColorModeValue('gray.200', 'gray.700'),
+        }}
+        href={'#'}>
+        {children}
+    </Link>
+);
+
+export default function Navbar() {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+
+    return (
+        <>
+            <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+                <Flex h={16} flexDirection={'row-reverse'} alignItems={'center'} justifyContent={'space-around'}>
+                    <IconButton
+                        size={'md'}
+                        icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+                        aria-label={'Open Menu'}
+                        display={{ md: 'none' }}
+                        onClick={isOpen ? onClose : onOpen}
+                    />
+                    <HStack spacing={8} alignItems={'center'}>
+                        <Box>Logo</Box>
+                        <HStack
+                            as={'nav'}
+                            spacing={4}
+                            display={{ base: 'none', md: 'flex' }}>
+                            {Links.map((link) => (
+                                <NavLink key={link}>{link}</NavLink>
+                            ))}
+                        </HStack>
+                    </HStack>
+                </Flex>
+
+                {isOpen ? (
+                    <Box pb={4} display={{ md: 'none' }}>
+                        <Stack as={'nav'} spacing={4}>
+                            {Links.map((link) => (
+                                <NavLink key={link}>{link}</NavLink>
+                            ))}
+                        </Stack>
+                    </Box>
+                ) : null}
+            </Box>
+
+            <Box p={4}>Main Content Here</Box>
+        </>
+    );
+}
