73f8abd06b30ba75ae8146a46cc7325ae741b256
Mukul Rana <mukulgenious123@gmail.com>
2023-10-05 17:33:08 +0530

    Create About Page

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Navbar } from './Navbar/Navbar';
 export { default as Footer } from './Footer/Footer';
 export { default as Form } from './Form/Form';
 export { default as CTA } from './CTA/CTA';
+export { default as About } from './About/About';
